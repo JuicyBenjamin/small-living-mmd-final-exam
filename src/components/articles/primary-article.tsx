902150a85dbeb01eee5df53475a251929990e38d
emma673r <90177139+emma673r@users.noreply.github.com>
2023-05-08 12:21:20 +0200

    test

---
+++ src/components/articles/primary-article.tsx
@@ -0,0 +1,15 @@
+import { component$ } from "@builder.io/qwik";
+
+export default component$(()=> {
+    return(
+        <>
+        <h1>This is the title</h1>
+        <h2>This is the subtitle</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos neque eius velit dignissimos nostrum ullam aliquid necessitatibus eaque impedit, dicta facilis eum aperiam et magnam alias fuga, quae cupiditate modi!
+        Quidem corrupti voluptas quisquam sed, saepe vero, est adipisci reiciendis aut optio dolores perferendis neque deserunt dolor! Enim magni ratione aliquid at vel dolorum quam officia sapiente, numquam voluptatum animi!
+        Quasi molestias magnam nisi dolores perferendis natus nam nemo in veniam corporis repudiandae distinctio illum, ab iste obcaecati nobis laudantium modi laboriosam provident dolore. Repellat blanditiis fuga dolor dolores doloribus.
+        Obcaecati at ex, repudiandae dolor qui ipsam sunt commodi nulla non odit modi, dolorem maiores iusto perspiciatis, tenetur iste sit. Nemo odit voluptas repellat tempore quis excepturi iusto accusantium et!
+        Necessitatibus doloribus aut similique quibusdam ab tenetur ullam minima labore aliquam, porro nemo sit autem doloremque molestiae accusantium veritatis magni earum neque, asperiores cupiditate deleniti? Amet nihil similique aspernatur nostrum.</p>
+        </>
+    )
+})
