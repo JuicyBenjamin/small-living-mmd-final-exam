736bba66b1b1e042f572864001713905813d2e35
emma673r <90177139+emma673r@users.noreply.github.com>
2023-05-08 12:06:35 +0200

    clean up start

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,30 +1,17 @@
 import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 
-import Counter from '~/components/starter/counter/counter';
+
 import Hero from '~/components/starter/hero/hero';
 import Infobox from '~/components/starter/infobox/infobox';
-import Starter from '~/components/starter/next-steps/next-steps';
+
 
 export default component$(() => {
   return (
     <>
       <Hero />
 
-      <div class="section bright">
-        <div class="container center">
-          <Starter />
-        </div>
-      </div>
 
-      <div class="section">
-        <div class="container center">
-          <h3>
-            You can <b>count</b> on me
-          </h3>
-          <Counter />
-        </div>
-      </div>
 
       <div class="section">
         <div class="container topics">
