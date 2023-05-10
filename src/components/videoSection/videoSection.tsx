11507c8a89d71181a8a1ede64c4c99f53c17dcf9
emma673r <90177139+emma673r@users.noreply.github.com>
2023-05-10 14:55:05 +0200

    video section

---
+++ src/components/videoSection/videoSection.tsx
@@ -0,0 +1,22 @@
+import { component$ } from "@builder.io/qwik";
+
+export default component$(() => {
+  return (
+    <section class={"flex justify-between gap-8 py-20 px-16 border-y-2 border-green-900"}>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/59KKxorhMD0"
+        title="YouTube video player 1"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      ></iframe>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/59KKxorhMD0"
+        title="YouTube video player 2"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      ></iframe>
+    </section>
+  );
+});
