1340d83b8d92661b0c63ff2a8c9a3fa1b5458265
emma673r <90177139+emma673r@users.noreply.github.com>
2023-05-24 10:34:36 +0200

    addign styles

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -6,27 +6,33 @@ export default component$(() => {
     <div class="w-full grid grid-cols-2">
       <div class="grid grid-rows-2 relative">
         <div class="relative flex flex-col justify-end">
-          <div class="absolute p-4">
-            <h2>Yolo</h2>
-            <p>Yolo some more</p>
-          </div>
-          <Image class="object-cover h-full" layout="fullWidth" src="https://picsum.photos/1920/1080" />
+          <h3 class="absolute p-4 bg-opacity-50">Title</h3>
+
+          <Image
+            class="object-cover h-full border-4 border-r-2 border-b-2 border-[--accent-green]"
+            layout="fullWidth"
+            src="https://picsum.photos/1920/1080"
+          />
         </div>
         <div class="relative flex flex-col justify-end">
-          <div class="absolute p-4">
-            <h2>Yolo</h2>
-            <p>Yolo some more</p>
-          </div>
-          <Image class="object-cover h-full" layout="fullWidth" src="https://picsum.photos/1920/1080" />
+          <h3 class="absolute p-4">Title</h3>
+
+          <Image
+            class="object-cover h-full border-4 border-t-2 border-r-2 border-[--accent-green]"
+            layout="fullWidth"
+            src="https://picsum.photos/1920/1080"
+          />
         </div>
       </div>
       <div class="relative flex flex-col justify-end">
-        <div class="absolute p-4">
-          <h2>Yolo</h2>
-          <p>Yolo some more</p>
-        </div>
-        <Image class="object-cover" layout="fullWidth" src="https://picsum.photos/1080/1920" />
+        <h3 class="absolute p-4">Title</h3>
+
+        <Image
+          class="object-cover border-4 border-l-2 border-[--accent-green]"
+          layout="fullWidth"
+          src="https://picsum.photos/1080/1920"
+        />
       </div>
     </div>
-  )
-})
+  );
+});
