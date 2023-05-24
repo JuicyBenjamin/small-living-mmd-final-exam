bbf803825de42a88a55f32eeb62bd7905512025c
emma673r <90177139+emma673r@users.noreply.github.com>
2023-05-24 14:16:25 +0200

    arranged height of hero elements

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -4,34 +4,42 @@ import { Image } from "@unpic/qwik";
 export default component$(() => {
   return (
     <div class="w-full grid grid-cols-2">
-      <div class="grid grid-rows-2 relative">
+      <div class="grid grid-rows-2 relative h-[90vh]">
         <div class="relative flex flex-col justify-end">
           <h3 class="absolute p-4 bg-opacity-50">Title</h3>
-
           <Image
+            src="https://picsum.photos/1000"
+            layout={"fullWidth"}
+            height={350}
+            // aspectRatio={1 / 1}
+            alt="Some picture"
             class="object-cover h-full border-4 border-r-2 border-b-2 border-[--accent-green]"
-            layout="fullWidth"
-            src="https://picsum.photos/1920/1080"
-          />
+          ></Image>
         </div>
         <div class="relative flex flex-col justify-end">
           <h3 class="absolute p-4">Title</h3>
 
           <Image
-            class="object-cover h-full border-4 border-t-2 border-r-2 border-[--accent-green]"
-            layout="fullWidth"
-            src="https://picsum.photos/1920/1080"
-          />
+            src="https://picsum.photos/800"
+            layout={"fullWidth"}
+            height={350}
+            // aspectRatio={1 / 1}
+            alt="Some picture"
+            class="object-cover h-full border-4 border-r-2 border-b-2 border-[--accent-green]"
+          ></Image>
         </div>
       </div>
-      <div class="relative flex flex-col justify-end">
+      <div class="relative flex flex-col justify-end h-[90vh]">
         <h3 class="absolute p-4">Title</h3>
 
         <Image
-          class="object-cover border-4 border-l-2 border-[--accent-green]"
-          layout="fullWidth"
-          src="https://picsum.photos/1080/1920"
-        />
+            src="https://picsum.photos/1200"
+            layout={"fullWidth"}
+            height={700}
+            aspectRatio={2 / 1}
+            alt="Some picture"
+            class="object-cover h-full border-4 border-r-2 border-b-2 border-[--accent-green]"
+          ></Image>
       </div>
     </div>
   );
