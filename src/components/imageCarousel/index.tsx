d74eeabfa30cc24eaab92f967a08e29d49a9f2e3
Benjamin Vinterberg <benj924c@stud.kea.dk>
2023-05-26 10:10:35 +0200

    wip

--- src/components/imageCarousel/index.tsx
+++ src/components/imageCarousel/index.tsx
@@ -1,8 +1,22 @@
-import { component$ } from "@builder.io/qwik";
+import { qwikify$ } from '@builder.io/qwik-react';
+import { Carousel } from 'react-responsive-carousel';
 
-export default component$(() => (
-  <>
-    <h1>yolo manner</h1>
-    <p>some text just because</p>
-  </>
-))
+export default qwikify$(() => {
+
+  return (
+    <Carousel>
+      <div>
+        <h1>yolo manner</h1>
+        <p>some text just because</p>
+      </div>
+      <div>
+        <h1>yolo manner</h1>
+        <p>some text just because</p>
+      </div>
+      <div>
+        <h1>yolo manner</h1>
+        <p>some text just because</p>
+      </div>
+    </Carousel>
+  );
+}, { eagerness: 'hover' });
