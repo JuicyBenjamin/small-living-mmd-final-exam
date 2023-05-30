3c930eaf32483de8e9fd0924dd076704193b7a40
emma673r <90177139+emma673r@users.noreply.github.com>
2023-05-30 11:01:00 +0200

    width on hashtags

--- src/components/hashtags/index.tsx
+++ src/components/hashtags/index.tsx
@@ -4,7 +4,7 @@ import type { categoriesContent } from "../article";
 export default component$<categoriesContent>(({categories}) => {
   return (
     <div class={"w-[100vw] bg-[--accent-lillac] my-10 py-2 grid"}>
-      <div class="flex gap-8 mx-auto max-w-[55ch] lg:w-[55ch]">
+      <div class="flex gap-8 mx-auto max-w-[75ch] lg:w-[75ch]">
              {categories.map((cat, key) => {
         if (cat == null) return;
         return <span class={"text-white"} key={key}><a class={"hover:underline cursor-pointer"} href={cat.url}>#{cat.name}</a></span>;
