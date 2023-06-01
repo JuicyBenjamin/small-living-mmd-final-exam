7a191638577d099165e3d70c03068a15d80ed30f
emma673r <90177139+emma673r@users.noreply.github.com>
2023-06-01 17:07:17 +0200

    video

--- src/components/videoSection/videoSection.tsx
+++ src/components/videoSection/videoSection.tsx
@@ -2,21 +2,22 @@ import { component$ } from "@builder.io/qwik";
 
 export default component$(() => {
   return (
-    <section class={"flex flex-col md:flex-row justify-between gap-8 py-20 md:px-16"}>
+    <section
+      class={"flex flex-col md:flex-row justify-around gap-8 py-20 md:px-16"}
+    >
       <iframe
-      class="w-full md:w-full px-4"
-        width="560"
-        height="315"
-        src="https://www.youtube-nocookie.com/embed/jfKfPfyJRdk"
-        title="YouTube video player"
-      
+        width="315"
+        height="560"
+        src="https://www.youtube.com/embed/oUD5SSpbZ4A"
+        title="Van Life Explainer - Small Living, KEA"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       ></iframe>
       <iframe
-            class="w-full md:w-full px-4"
-        width="560"
-        height="315"
-        src="https://www.youtube-nocookie.com/embed/jfKfPfyJRdk"
-        title="YouTube video player"
+        width="315"
+        height="560"
+        src="https://www.youtube.com/embed/oUD5SSpbZ4A"
+        title="Van Life Explainer - Small Living, KEA"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       ></iframe>
     </section>
   );
